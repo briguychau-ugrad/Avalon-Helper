f4a39c15ceb07f845f580e40d42b113df20845e7
Brian Chau <me@brianchau.ca>
2014-04-08 19:35:41 -0700

    Add simple switch to new activity

--- Avalon Helper/src/main/java/ca/brianchau/easy/avalonhelper/MainActivity.java
+++ Avalon Helper/src/main/java/ca/brianchau/easy/avalonhelper/MainActivity.java
@@ -1,15 +1,30 @@
 package ca.brianchau.easy.avalonhelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+/**
+ * Created by Brian on 2014-04-08.
+ */
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_main_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SetupActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
---
+++ Avalon Helper/src/main/java/ca/brianchau/easy/avalonhelper/SetupActivity.java
@@ -0,0 +1,17 @@
+package ca.brianchau.easy.avalonhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Brian on 2014-04-08.
+ */
+public class SetupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
